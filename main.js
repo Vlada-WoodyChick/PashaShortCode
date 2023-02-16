a563c1229c3746854cbc94e8453f1b9d114b761b
Vlada-WoodyChick <uchusa@mail.ru>
2023-02-16 16:52:45 +0300

    another commit

---
+++ main.js
@@ -0,0 +1,73 @@
+// //number
+// //bigint
+// //string
+// //symbol
+// //object
+// //null
+// //undefind
+// //boolean
+
+// // let a = [1, 2, 3, 6]
+
+
+// // const second = a[1]
+// // console.log(a.length );
+
+// // const sum =function( a, b) {
+// //   return a+b;
+// // }
+// // console.log( sum(2, 3) )
+
+// // 2 + '2'
+// // console.log(2 + '2')
+// // console.log('2' + 2 - 2);
+
+// // const obj = {
+// //   color: 'red',
+// //   size: '1800mm',
+// //   }
+// // console.log(obj.size);
+// // obj.price = 2500;
+// // console.log(obj);
+
+// // let a = [1, 2, 6, 4];
+// // a.push(7);
+// //  console.log(a);
+// // const a = 1;
+
+// // if( a >= 2 ){
+// //  console.log('yes');
+// // } else {console.log('no')}
+// console.log('Hello, world!');
+
+// // alert('Hello')
+// // [1, 2].forEach(alert);  //Uncaught TypeError: alert(...) is undefined
+
+
+//  //  переменные
+//  let name;
+//  let admin; 
+//  name = 'John';
+//  admin = name;
+//  console.log(admin);
+
+
+// //   JSON
+const obj = {   // {name: 'John', age: 23}
+    name: 'John',   // typeof obj - 'object'
+    age: 23,
+    job: undefined,
+    foo: () => {},
+}
+obj.iam = obj;
+
+// const jsonObj = JSON.stringify(obj); // '{"name":"John","age":23}' - typeof jsonObj - 'string'
+// // // ключи свойств с двойными кавычками
+// JSON.stringify( [1, 2, 3] ); // '[1,2,3]'; // // https://jsonformatter.org/
+// const parsedObj = JSON.parse(jsonObj); // {name: 'John', age: 23} object
+
+/*O, My God!*/
+console.log ('Learn git');
+console.log ('Learn git');
+console.log ('Learn git');
+console.log ('Learn git');
